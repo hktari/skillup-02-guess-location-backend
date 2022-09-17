7306d395d10b4acfc3754582fecd290f5104d6c0
hktari <bkamnik1995@gmail.com>
2022-09-17 08:39:30 +0200

    add signup test calls AuthService

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,32 +2,43 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { SignupDto } from './dto/signup.dto';
 
 const moduleMocker = new ModuleMocker(global);
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const signupResult = { id: '0', email: 'test@example.com', firstName: 'john', lastName: 'scott', password: 'secret', imageUrl: 'https://...' }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService]
+      controllers: [AuthController]
     })
-    .useMocker((token) => {
-      // if (token === CatsService) {
-      //   return { findAll: jest.fn().mockResolvedValue(results) };
-      // }
-      if (typeof token === 'function') {
-        const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
-        const Mock = moduleMocker.generateFromMetadata(mockMetadata);
-        return new Mock();
-      }
-    }).compile();
+      .useMocker((token) => {
+        console.log('mocker')
+        if (token === AuthService) {
+          console.log('AuthService token')
+          return { signup: jest.fn().mockResolvedValue(signupResult) };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      }).compile();
 
+    console.log('compiled')
     controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('signup returns AuthService result', async () => {
+    const signupDto: SignupDto = { email: 'test@example.com', firstName: 'john', lastName: 'scott', password: 'secret', imageBase64: '123qwe==' }
+
+    expect(await controller.signup(signupDto)).toBe(signupResult)
+  })
 });
