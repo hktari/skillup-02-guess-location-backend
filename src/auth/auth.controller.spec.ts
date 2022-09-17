14a396d3ff25908d20b5c7b35544de3e01646906
hktari <bkamnik1995@gmail.com>
2022-09-17 08:47:33 +0200

    auth controller login calls AuthService

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -3,24 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { SignupDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 const moduleMocker = new ModuleMocker(global);
 
 describe('AuthController', () => {
   let controller: AuthController;
-
-  const signupResult = { id: '0', email: 'test@example.com', firstName: 'john', lastName: 'scott', password: 'secret', imageUrl: 'https://...' }
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController]
+      controllers: [AuthController],
+      providers: [AuthService]
     })
       .useMocker((token) => {
         console.log('mocker')
-        if (token === AuthService) {
-          console.log('AuthService token')
-          return { signup: jest.fn().mockResolvedValue(signupResult) };
-        }
         if (typeof token === 'function') {
           const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
           const Mock = moduleMocker.generateFromMetadata(mockMetadata);
@@ -30,6 +28,7 @@ describe('AuthController', () => {
 
     console.log('compiled')
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
@@ -38,7 +37,19 @@ describe('AuthController', () => {
 
   it('signup returns AuthService result', async () => {
     const signupDto: SignupDto = { email: 'test@example.com', firstName: 'john', lastName: 'scott', password: 'secret', imageBase64: '123qwe==' }
+    const signupResult: UserEntity = { id: '0', email: 'test@example.com', firstName: 'john', lastName: 'scott', password: 'secret', imageUrl: 'https://...', locations: [] }
+    jest.spyOn(service, 'signup').mockResolvedValue(signupResult)
 
     expect(await controller.signup(signupDto)).toBe(signupResult)
+  })
+
+  describe('login', () => {
+    it('should return AuthService login result', async () => {
+      const result = { access_token: 'asldkasj912ijk1' }
+      jest.spyOn(service, 'login').mockResolvedValue(result);
+
+      const loginDto: LoginDto = { email: 'test@example.com', password: 'secret' }
+      expect(await controller.login(loginDto)).toBe(result)
+    })
   })
 });
