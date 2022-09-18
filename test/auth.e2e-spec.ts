f8dc4d09f80849b6346343f0772b5fb7dbf87b00
hktari <bkamnik1995@gmail.com>
2022-09-18 10:22:56 +0200

    login request test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -5,32 +5,51 @@ import { AuthModule } from '../src/auth/auth.module';
 import { AuthService } from '../src/auth/auth.service';
 import { DatabaseModule } from '../src/database/database.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { Repository } from 'typeorm'
+import { UserEntity } from '../src/user/entities/user.entity'
+import { UserRepository } from '../src/common/constants';
 
 describe('Auth', () => {
   let app: INestApplication;
-  let catsService = { findAll: () => ['test'] };
+  let authService = { findAll: () => ['test'] };
+  let userRepository: Repository<UserEntity>;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AuthModule, DatabaseModule, ConfigModule.forRoot({ envFilePath: '../test.env', isGlobal: true, })],
     })
-      .overrideProvider(AuthService)
-      .useValue(catsService)
+      // .overrideProvider(AuthService)
+      // .useValue(authService)
       .compile();
 
     app = moduleRef.createNestApplication();
     await app.init();
+
+    userRepository = moduleRef.get<Repository<UserEntity>>(UserRepository)
   });
 
-  it(`/POST auth/login`, () => {
-    const result = { access_token: 'asldkasj912ijk1' }
-    catsService['login'] = () => result
+  it(`/POST auth/login`, (done) => {
+    const loginUser = userRepository.create()
+    loginUser.email = 'test@example.com'
+    loginUser.firstName = 'scott'
+    loginUser.lastName = 'johnson'
+    loginUser.password = 'secret'
+    userRepository.save(loginUser)
 
+    const loginDto = {
+      email: loginUser.email,
+      password: loginUser.password
+    }
+
+    
     return request(app.getHttpServer())
       .post('/auth/login')
-      .expect(200)
-      .expect(result);
+      .send(loginDto)
+      .then((response) => {
+        expect(response.statusCode).toBe(201)
+        expect(response.body).toHaveProperty('access_token')
+        done()
+      });
   });
 
   afterAll(async () => {
