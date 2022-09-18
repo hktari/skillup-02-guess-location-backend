d854e429f4e027e9f49338335d4d87fb46bdbbff
hktari <bkamnik1995@gmail.com>
2022-09-19 00:53:47 +0200

    fix import path and address faker

--- test/data/factory/guess-location.factory.ts
+++ test/data/factory/guess-location.factory.ts
@@ -3,7 +3,7 @@ import { GuessLocationEntity } from '../../../src/location/entities/guess-locati
 
 export default setSeederFactory(GuessLocationEntity, (faker) => {
     const guess = new GuessLocationEntity();
-    guess.address = `${faker.address.street(), faker.address.city(), faker.address.country()}`;
+    guess.address = `${faker.address.street()}, ${faker.address.city()}, ${faker.address.country()}`;
     guess.lat = +faker.address.latitude()
     guess.lng = +faker.address.longitude()
     return guess;
--- test/data/factory/location.factory.ts
+++ test/data/factory/location.factory.ts
@@ -3,7 +3,7 @@ import { LocationEntity } from '../../../src/location/entities/location.entity';
 
 export default setSeederFactory(LocationEntity, (faker) => {
     const location = new LocationEntity();
-    location.address = `${faker.address.street(), faker.address.city(), faker.address.country()}`;
+    location.address = `${faker.address.street()}, ${faker.address.city()}, ${faker.address.country()}`;
     location.imageUrl = faker.internet.url()
     location.lat = +faker.address.latitude()
     location.lng = +faker.address.longitude()
