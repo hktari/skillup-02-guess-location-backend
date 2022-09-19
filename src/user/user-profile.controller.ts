410eb8e82e2707579a51914d7add931f431000cd
hktari <bkamnik1995@gmail.com>
2022-09-19 02:15:10 +0200

    add updateMyPassword handler

--- src/user/user-profile.controller.ts
+++ src/user/user-profile.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Body, ClassSerializerInterceptor, Controller, Get, Param, Post, UseInterceptors, UseGuards, Put } from '@nestjs/common';
+import { Response, Request, Body, ClassSerializerInterceptor, Controller, Get, Param, Post, UseInterceptors, UseGuards, Put, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Response as ExResponse, Request as ExRequest } from 'express';
 import { CreateUserDto } from './dto/CreateUserDto';
 import { UpdateUserProfileDto } from './dto/UpdateUserProfileDto';
 import { User } from './user.interface';
@@ -20,11 +21,18 @@ export class UserController {
     @Put('my-profile')
     updateMyProfile(@Request() req, @Body() { firstName, lastName, imageBase64 }: UpdateUserProfileDto) {
         // todo: if imageBase64 get imageUrl
+
         return this.userService.update({
             email: req.email,
             firstName: firstName ?? req.firstName,
             lastName: lastName ?? req.lastName,
             imageUrl: req.imageUrl
         })
+    }
+
+    @Put('my-profile/password')
+    async updateMyPassword(@Request() req, @Response() res: ExResponse, @Body('password', new ValidationPipe()) password: string) {
+        await this.userService.setPassword(req.email, password)
+        return res.sendStatus(200)
     }
 }
