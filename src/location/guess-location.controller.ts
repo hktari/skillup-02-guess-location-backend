e40ed0db7fcc387bb79bc65386f762979f3d531b
hktari <bkamnik1995@gmail.com>
2022-09-20 15:16:06 +0200

    implement guess location handler

--- src/location/guess-location.controller.ts
+++ src/location/guess-location.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Param, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UserEntity } from '../user/entities/user.entity';
 import { User } from '../common/decorators/user.decorator'
 import { GuessLocationDto } from './dto/GuessLocationDto';
+import { AuthGuard } from '@nestjs/passport';
+import { LocationService } from './location.service';
+import { create } from 'domain';
 
 @Controller('location/guess')
+@UseInterceptors(ClassSerializerInterceptor)
 export class GuessLocationController {
+    constructor(private locationService: LocationService) { }
 
     @Post(':id')
+    @UseGuards(AuthGuard('jwt'))
     create(@User() user: UserEntity, @Body() guessLocationDto: GuessLocationDto, @Param('id') id: string) {
-        return `guess location ${id}: ${typeof guessLocationDto.lat}`
+        return this.locationService.guessLocation(id, user.id, guessLocationDto)
     }
 }
